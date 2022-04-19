11f461bd34acdc78a2e622d437691e4de6a2ceb0
P-Yevhenii <py@atticlab.net>
2022-04-19 15:23:51 +0300

    refactor code

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -5,7 +5,7 @@ set -o pipefail
 
 projectPath=$(cd "$(dirname "${0}")" && cd ../ && pwd)
 
-docker run -v "$projectPath":/code \
+docker run --rm -v "$projectPath":/code \
   --mount type=volume,source="$(basename "$projectPath")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
   cosmwasm/workspace-optimizer:0.12.6
